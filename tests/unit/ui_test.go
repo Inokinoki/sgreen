0ad37bf6020faec11528112bbcd30313d915d2fa
Inoki <veyx.shaw@gmail.com>
2026-03-29 17:02:30 +0200

    test: Add test for DetectTerminalCapabilities

--- tests/unit/ui_test.go
+++ tests/unit/ui_test.go
@@ -212,3 +212,32 @@ func TestShowHelp(t *testing.T) {
 		t.Errorf("ShowHelp output should contain 'sgreen Key Bindings'")
 	}
 }
+
+func TestDetectTerminalCapabilities(t *testing.T) {
+	tests := []struct {
+		name     string
+		term     string
+		colorterm string
+		wantColor bool
+	}{
+		{"xterm-256color", "xterm-256color", "", true},
+		{"screen-256color", "screen-256color", "", true},
+		{"xterm", "xterm", "", false},
+		{"dumb", "dumb", "", false},
+		{"truecolor term", "xterm", "truecolor", true},
+		{"24bit term", "xterm", "24bit", true},
+		{"empty term", "", "truecolor", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			os.Setenv("TERM", tt.term)
+			os.Setenv("COLORTERM", tt.colorterm)
+			caps := ui.DetectTerminalCapabilities()
+
+			if caps.HasColor != tt.wantColor {
+				t.Errorf("DetectTerminalCapabilities() HasColor = %v, want %v", caps.HasColor, tt.wantColor)
+			}
+		})
+	}
+}
